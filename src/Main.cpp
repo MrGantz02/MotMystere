8f3992e61ffd942acd831a31f26fcde187a0b291
MrGantz02 <mrgantz02@gmail.com>
2014-03-23 12:02:55 +0100

    Update #1
    Ajoute la possibilité de refaire une partie ou de quitter, saute des lignes pour que le jouer 2 ne voit pas ce qu'à écrit le joueur 1.

--- src/Main.cpp
+++ src/Main.cpp
@@ -22,29 +22,48 @@ string randChar(string mWord)
 int main()
 {
 	srand(time(0));
+	bool game(true);
 
-	string mysteryWord;
-	string tryWord;
-	int randNumber(0);
-	string mingledWord;
-
-	cout << "JOUEUR 1 - Ecrivez le mot secret : ";
-	cin >> mysteryWord;
-
-	while (tryWord != mysteryWord)
+	do
 	{
-		mingledWord = randChar(mysteryWord);
+		system("cls");
 
-		cout << endl << endl << "JOUEUR 2 - Trouvez le mot secret ! Voici les lettres melangees : " << mingledWord << endl << "Votre choix : ";
-		cin >> tryWord;
+		string mysteryWord;
+		string tryWord;
+		int randNumber(0);
+		string mingledWord;
+		char retry;
 
-		if (tryWord != mysteryWord)
+		cout << "JOUEUR 1 - Ecrivez le mot secret : ";
+		cin >> mysteryWord;
+
+		while (tryWord != mysteryWord)
 		{
-			cout << endl << "Ce n'est pas le mot !" << endl << endl;
+			mingledWord = randChar(mysteryWord);
+
+			cout << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl;
+			cout << "JOUEUR 2 - Trouvez le mot secret ! Voici les lettres melangees : " << mingledWord << endl << "Votre choix : ";
+			cin >> tryWord;
+
+			if (tryWord != mysteryWord)
+			{
+				cout << endl << "Ce n'est pas le mot !" << endl << endl;
+			}
 		}
-	}
 
-	cout << endl << "Bravo !" << endl << endl;
+		cout << endl << "Bravo ! Voulez-vous refaire une partie ? Y en majuscule pour oui, autre chose pour non : ";
+		cin >> retry;
+		if (retry == 'Y')
+		{
+			game = true;
+		}
+		else
+		{
+			game = false;
+		}
+		
+	} while (game);
 
+	cout << endl;
 	system("PAUSE");
 }
